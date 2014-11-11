5c6e93341268ed867ed654761890b5ef2efe8099
tew88 <tomew88@gmail.com>
2014-11-11 19:20:43 +0000

    Made User domain immutable.

--- src/main/java/io/tew88/rook/domain/User.java
+++ src/main/java/io/tew88/rook/domain/User.java
@@ -1,20 +1,19 @@
 package io.tew88.rook.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class User {
 
-    private int id;
-    private String name;
-    private String email;
+    private final int id;
+    private final String name;
+    private final String email;
     
-    public User(final int id, final String name, final String email) {
+    @JsonCreator
+    public User(@JsonProperty("id") final int id, @JsonProperty("name") final String name, @JsonProperty("email") final String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-    }
-    
-    public User() {
     }
 
     @JsonProperty
